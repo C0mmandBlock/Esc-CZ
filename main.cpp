086bd19cb2171bd69e3c29e432dd1c3c93c08cad
ExcelMaster <88095744+C0mmandBlock@users.noreply.github.com>
2024-04-29 23:07:57 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,8 @@
 int main() {
   SDL_Window* window = SDL_CreateWindow("Esc CZ", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 1000, 0);
   SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  int running = 1;
+  while (running) {
+  }
   return 0;
 }
