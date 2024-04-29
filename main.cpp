b7ccaf5d1c9441a86df773058a83534bffac0d61
ExcelMaster <88095744+C0mmandBlock@users.noreply.github.com>
2024-04-29 23:06:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,6 @@
 
 int main() {
   SDL_Window* window = SDL_CreateWindow("Esc CZ", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 1000, 0);
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
   return 0;
 }
