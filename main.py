a693a32ee4f48adcfe82fd6a17aa2ed87a8e4d5b
ExcelMaster <88095744+C0mmandBlock@users.noreply.github.com>
2024-05-01 11:10:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,17 +12,20 @@ GRAY = (127, 127, 127) #🇺🇸🦅
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
 
 mapData = {
-    "color": {(0,0): GRAY, (0,1): RED, (0,2): GRAY, (1,0): GRAY, (1,1): RED, (1,2): GRAY, (2,0): GRAY, (2,1): GRAY, (2,2): RED},
+    "color1": {(0,0): GRAY, (0,1): RED, (0,2): GRAY, (1,0): GRAY, (1,1): RED, (1,2): GRAY, (2,0): GRAY, (2,1): GRAY, (2,2): RED},
+    "color2": {(0,0): RED, (0,1): GRAY, (0,2): RED, (1,0): RED, (1,1): GRAY, (1,2): RED, (2,0): RED, (2,1): RED, (2,2): GRAY},
+    "color3": {(0,0): GREEN, (0,1): BLUE, (0,2): BLUE, (1,0): GREEN, (1,1): BLUE, (1,2): GREEN, (2,0): BLUE, (2,1): BLUE, (2,2): BLUE},
     "height": {(0,0): 0, (0,1): 0, (0,2): 0, (1,0): 0, (1,1): 0, (1,2): 0, (2,0): 0, (2,1): 0, (2,2): 0}
 }
 
 tile_width = 64*3
 tile_height = 32*3
 
-map_width = 1
-map_height = 1
+map_width = 3
+map_height = 3
 
 offsetX = 320
 offsetY = 20
@@ -35,12 +38,19 @@ def draw_tiles():
         for x in range(map_width):
             iso_pos = (x, y)
             screen_pos = iso_to_screen(iso_pos)
-            pygame.draw.polygon(screen, mapData["color"][(x,y)], [
-                (screen_pos[0], screen_pos[1] + tile_height / 2),
-                (screen_pos[0] + tile_width / 2, screen_pos[1]),
-                (screen_pos[0] + tile_width, screen_pos[1] + tile_height / 2),
-                (screen_pos[0] + tile_width / 2,screen_pos[1] + tile_height)
-            ],2)
+            a,b,c,d,e,f,g = (screen_pos[0]+tile_width/2,screen_pos[1]+tile_height*2),(screen_pos[0]+tile_width/2,screen_pos[1]+tile_height),(screen_pos[0]+tile_width,screen_pos[1]+tile_height/2),(screen_pos[0]+tile_width,screen_pos[1]+tile_height*1.5),(screen_pos[0],screen_pos[1]+tile_height/2),(screen_pos[0],screen_pos[1]+tile_height*1.5),(screen_pos[0]+tile_width/2,screen_pos[1])
+            pygame.draw.polygon(screen, mapData["color1"][(x,y)], [e, g, c, b])
+            pygame.draw.polygon(screen, mapData["color2"][(x,y)], [b, a, f, e])
+            pygame.draw.polygon(screen, mapData["color3"][(x,y)], [b, a, d, c])
+            pygame.draw.line(screen, BLACK, a, b, 2)
+            pygame.draw.line(screen, BLACK, a, f, 2)
+            pygame.draw.line(screen, BLACK, a, d, 2)
+            pygame.draw.line(screen, BLACK, b, c, 2)
+            pygame.draw.line(screen, BLACK, e, g, 2)
+            pygame.draw.line(screen, BLACK, g, c, 2)
+            pygame.draw.line(screen, BLACK, e, f, 2)
+            pygame.draw.line(screen, BLACK, e, b, 2)
+            pygame.draw.line(screen, BLACK, d, c, 2)
 
 def main():
     clock = pygame.time.Clock()
